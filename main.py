3168ee518ea4976f8457160d3f5f019b7b981cfd
cauaa23 <116275269+cauaa23@users.noreply.github.com>
2022-12-10 23:39:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from src.maquina import Maquina
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+maquina = Maquina(3,5)
+print(maquina.getSizeEspirais())
+print(maquina.espirais)
